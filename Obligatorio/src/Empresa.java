193e68a6a48895261d1d2363f5aa1f86d0fb0c2c
telematica <telematica@E101-4Q22.sam.um.edu.uy>
2018-05-28 19:39:57 -0300

    Primera subida

---
+++ Obligatorio/src/Empresa.java
@@ -0,0 +1,25 @@
+
+public class Empresa {
+	
+	private String name;
+	
+	private String ruc;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRuc() {
+		return ruc;
+	}
+
+	public void setRuc(String ruc) {
+		this.ruc = ruc;
+	}
+	
+	
+}
---
+++ Obligatorio/src/Producto.java
@@ -0,0 +1,34 @@
+
+public class Producto {
+	
+	private String name;
+	
+	private String fantasyName;
+	
+	private String status;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFantasyName() {
+		return fantasyName;
+	}
+
+	public void setFantasyName(String fantasyName) {
+		this.fantasyName = fantasyName;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
